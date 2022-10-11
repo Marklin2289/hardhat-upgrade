1137169d72054514916da8e0f7e0c76d148ee5c3
marklin2289 <marklin2289@gmail.com>
2022-10-10 21:15:52 -0700

    yarn hardhat compile

--- contracts/Box.sol
+++ contracts/Box.sol
@@ -2,17 +2,17 @@
 pragma solidity ^0.8.8;
 
 contract Box {
-    uint256 internal value
+    uint256 internal value;
 
-    event ValueChanged(uint256 newValue)
+    event ValueChanged(uint256 newValue);
 
     function store(uint256 newValue) public {
         value = newValue;
-        emit ValueChanged(newValue)
+        emit ValueChanged(newValue);
     }
 
     function retrieve() public view returns(uint256) {
-        return value
+        return value;
     }
 
     function version() public pure returns(uint256) {
