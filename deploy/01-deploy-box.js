4fb553aff0d80875d08fcf05cea9068a883f4802
marklin2289 <marklin2289@gmail.com>
2022-10-10 21:39:56 -0700

    01-deploy-box.js

---
+++ deploy/01-deploy-box.js
@@ -0,0 +1,38 @@
+const { developmentChains, VERIFICATION_BLOCK_CONFIRMATIONS } = require("../helper-hardhat-config")
+const { network, ethers } = require("hardhat")
+const { verify } = require("util/verify")
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+
+    const waitBlockConfirmations = developmentChains.includes(network.name)
+        ? 1
+        : VERIFICATION_BLOCK_CONFIRMATIONS
+
+    log("----------------------------------------------------")
+
+    const box = await deploy("Box", {
+        from: deployer,
+        args: [],
+        log: true,
+        waitBlockConfirmations: waitBlockConfirmations,
+        proxy: {
+            proxyContract: "OpenZeppelinTransparentProxy",
+            viaAdminContract: {
+                name: "BoxProxyAdmin",
+                artifact: "BoxProxyAdmin",
+            },
+        },
+    })
+
+    // Verify the deployment
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        logger("Verifying...")
+        const boxAddress = (await ethers.getContractAt("Box_Implementation")).address
+        await verify(boxAddress, [])
+    }
+    log("----------------------------------------------------------------")
+}
+
+module.exports.tags = ["all", "box"]
